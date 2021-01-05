2afb0d322361ed2913afbab9e183130deecc97f9
OptimisticSide <47266822+optimisticside@users.noreply.github.com>
2021-01-05 13:58:03 -0800

    Added dadjoke command

--- src/commands/fun/dadJoke.js
+++ src/commands/fun/dadJoke.js
@@ -1,23 +1,22 @@
 const { MessageEmbed } = require("discord.js");
 const got = require("got");
 
-class DogCommand {
+class DadJokeCommand {
     /**
-     * dog command
+     * dadJoke command
      * @param catalyst the framework
      * @param message the message that called the command
      * @param args any arguments provided in the message
      */
     async run(catalyst, message, args) {
-        got("https://dog.ceo/api/breeds/image/random").then(res => {
+        got("https://icanhazdadjoke.com/", { headers: { "Accept": "application/json" } }).then(res => {
             const content = JSON.parse(res.body);
-            const url = content.message;
+            const joke = content.joke;
 
             /* create embed */
             const embed = new MessageEmbed()
-                .setTitle("Dog")
-                .setURL(url)
-                .setImage(url)
+                .setTitle("Dad joke")
+                .setDescription(joke)
                 .setColor(catalyst.config.DEFAULT_COLOR)
                 .setFooter(message.author.tag, message.author.displayAvatarURL);
 
@@ -45,10 +44,9 @@ class DogCommand {
         this.catalyst = catalyst;
 
         /* command info */
-        this.name = "dog";
-        this.description = "Gets a random image of a dog";
-        this.aliases = ["getDog", "woof"];
+        this.name = "dadJoke";
+        this.description = "Gets a random dad joke";
     }
 };
 
-module.exports = DogCommand;
+module.exports = DadJokeCommand;
