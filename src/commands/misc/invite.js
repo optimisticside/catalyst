d64ea4b1ac4a5aa6bd650d2d45c857810cf57fc0
OptimisticSide <47266822+optimisticside@users.noreply.github.com>
2021-01-04 20:44:03 -0800

    Added invite command

---
+++ src/commands/misc/invite.js
@@ -0,0 +1,33 @@
+const { MessageEmbed } = require("discord.js");
+
+class InviteCommand {
+    /**
+     * invite ncommand
+     * @param catalyst the framework
+     * @param message the message that called the command
+     * @param args any arguments provided in the message
+     */
+    async run(catalyst, message, args) {
+        /* create embed */
+        const embed = new MessageEmbed()
+            .setTitle("Invite")
+            .setURL(catalyst.config.INVITE_URL)
+            .setDescription("Here's the link to invite me")
+            .setColor(catalyst.config.DEFAULT_COLOR)
+            .setFooter(message.author.tag, message.author.displayAvatarURL);
+
+        /* send embed */
+        message.channel.send(embed);
+    };
+
+    constructor(catalyst) {
+        this.catalyst = catalyst;
+
+        /* command info */
+        this.name = "invite";
+        this.description = `Gets the link to invite ${catalyst.config.NAME} to your server`;
+        this.aliases = ["getInviet"];
+    }
+};
+
+module.exports = InviteCommand;
