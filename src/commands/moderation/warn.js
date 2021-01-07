7459ed1cb8b2b2d9c9e10a5dac8a45fef4973433
OptimisticSide <47266822+optimisticside@users.noreply.github.com>
2021-01-06 20:42:20 -0800

    Updated warn GIF

---
+++ src/commands/moderation/warn.js
@@ -0,0 +1,56 @@
+const { MessageEmbed } = require("discord.js");
+
+class WarnCommand {
+    /**
+     * warn command
+     * @param catalyst the framework
+     * @param message the message that called the command
+     * @param args any arguments provided in the message
+     */
+    async run(catalyst, message, args) {
+        /* format arguments */
+        var target = message.mentions.members.first();
+        var reason = args.slice(1).join(catalyst.config.SPLIT_KEY) || "Not provided";
+
+        /* throw error if no target provided */
+        if (!target) {
+            /* create embed */
+            var embed = new MessageEmbed()
+                .setTitle("Warn")
+                .setDescription("❗ No user provided")
+                .setColor(catalyst.config.FAIL_COLOR)
+                .setFooter(message.author.tag, message.author.displayAvatarURL);
+            
+            /* send embed and return */
+            return message.channel.send(embed);
+        }
+
+        /* thrrow error if lacking permissions */
+        if (!target.kickable) {
+            /* create embed */
+            var embed = new MessageEmbed()
+                .setTitle("Warn")
+                .setDescription(`❗ Lacking permissions to warn ${target.user.tag}`)
+                .setColor(catalyst.config.FAIL_COLOR)
+                .setFooter(message.author.tag, message.author.displayAvatarURL);
+
+            /* send embed and return */
+            return message.channel.send(embed);
+        }
+
+        /* actually warn the user */
+        message.channel.send("https://media.tenor.co/videos/eceb7080cd43aac3c6ba00c0c518ad7e/mp4");
+    };
+
+    constructor(catalyst) {
+        this.catalyst = catalyst;
+
+        /* command info */
+        this.name = "warn";
+        this.description = "Warns a user";
+        this.perms = ["MANAGE_GUILD"];
+        this.argFormat = ["user"];
+    }
+};
+
+module.exports = WarnCommand;
