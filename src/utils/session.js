52b5b749cc4ab90753cfdb386dd0c7f9da0625ed
OptimisticSide <47266822+optimisticside@users.noreply.github.com>
2021-07-04 08:30:29 -0700

    Added documentation and support for ephemeral replies

--- src/utils/session.js
+++ src/utils/session.js
@@ -5,6 +5,11 @@ const { Message, APIMessage } = require('discord.js');
  * user.
  */
 class Session {
+	/**
+	 * Creates a Session.
+	 * @param {Discord.Client} client The client having this intearction.
+	 * @param {Discord.Message|Interaction} given The given data.
+	 */
 	constructor(client, given) {
 		const isSlash = !(given instanceof Message);
 		if (isSlash) {
@@ -22,12 +27,21 @@ class Session {
 		this.client = client;
 	}
 
+	/**
+	 * Sends raw data through the interaction.
+	 * @param {any} data The data to send.
+	 */
 	sendRaw(data) {
 		const interaction = this.interaction;
 		return this.client.api.interactions(this.interaction.id, this.interaction.token)
 			.callback.post(data);
 	}
 
+	/**
+	 * Formats any object (embed, attachment, etc).
+	 * @param {Object} object The object to format.
+	 * @returns {Object} the foramtted object.
+	 */
 	async formatObject(object) {
 		const { data, files } = await APIMessage.create(
 			this.client.channels.resolve(this.interaction.channel_id),
@@ -37,6 +51,11 @@ class Session {
 		return { ...data, files };
 	}
 
+	/**
+	 * Replies to the user through the interaction.
+	 * @param {any} response The data to reply with.
+	 * @returns {Discord.Message} The reply message.
+	 */
 	async respond(response) {
 		if (!this.isSlash) {
 			return this.message.channel.send(response);
@@ -47,6 +66,23 @@ class Session {
 		}
 		await this.sendRaw({ data: { type: 4, data } });
 		return this.channel && this.channel.lastMessage;
+	}
+
+	/**
+	 * Replies to the user ephemerally.
+	 * Only the user will be able to see the reply.
+	 * @param {any} response The data to reply with.
+	 */
+	async respondEp(response) {
+		if (!this.isSlash) {
+			return this.message.channel.send(response);
+		}
+		var data = { content: response };
+		if (typeof response === 'object') {
+			data = await this.formatObject(response);
+		}
+		data.ephemeral = true;
+		return await this.sendRaw({ data: { type: 4, data } });
 	}
 }
 
