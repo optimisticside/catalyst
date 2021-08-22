c2e764871da9f147d2d8e3b3c2eb6b814c34d95d
OptimisticSide <47266822+optimisticside@users.noreply.github.com>
2021-08-22 11:03:07 -0700

    Delete src directory

--- src/utils/exec.js
+++
@@ -1,9 +0,0 @@
-/**
- * Requires a file in a promise for clean
- * error handling.
- * @param {string} filePath The path to the file to require.
- * @returns {Promise<any>} The data exported by the file.
-*/
-module.exports = async filePath => {
-	return require(filePath);
-}
