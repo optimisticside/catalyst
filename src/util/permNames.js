16551d5d010b35b160d44c2aee8c0fb7ec029dbe
optimisticside <vijaysh312@gmail.com>
2021-10-30 19:06:58 +0000

    Revert "Public Release"
    This reverts commit 2555bf2d96b9b6c0ca5080cb88beb95a7fabdb19.

--- src/util/permNames.js
+++
@@ -1,7 +0,0 @@
-const { Permissions } = require('discord.js');
-
-module.exports = (perm) => {
-  // This is a cheap and lazy hack but it's great.
-  const perms = Object.entries(Permissions.FLAGS);
-  return perms.map(([ key, val ]) => val === perm);
-};
