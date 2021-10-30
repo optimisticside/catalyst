6a3fb805ab7a6a740138de09ba94b2ca92ed5460
optimisticside <47266822+optimisticside@users.noreply.github.com>
2021-10-30 20:49:32 +0000

    Created mkdocs files

--- src/core/shard.js
+++ src/core/shard.js
@@ -12,27 +12,25 @@ const requireAsync = async f => require(f); //promisify(require.bind(this));
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] });
 const modules = {};
 
-async function loadModules(files) {
-  await Promise.all(files.map(async file => {
-    const module = await requireAsync(file).catch(err => {
-      const fileName = path.basename(file, path.extname(file));
-      console.error(`Unable to load ${fileName} module: ${err}`);
-    });
-    const object = new module(client);
-    modules[object.name] = object;
-    client[object.name] = object;
-    /*
-    let module;
-    try {
-      module = require(file);
-    } catch(err) {
-      console.error(`Unable to load module ${file}: ${err}`)
-    }
-    let object = new module(client);
-    modules[object.name] = object;
-    client[object.name] = object;
-    */
-  }));
+async function loadModule(file) {
+  const module = await requireAsync(file).catch(err => {
+    const fileName = path.basename(file, path.extname(file));
+    console.error(`Unable to load ${fileName} module: ${err}`);
+  });
+  const object = new module(client);
+  modules[object.name] = object;
+  client[object.name] = object;
+  /*
+  let module;
+  try {
+    module = require(file);
+  } catch(err) {
+    console.error(`Unable to load module ${file}: ${err}`)
+  }
+  let object = new module(client);
+  modules[object.name] = object;
+  client[object.name] = object;
+  */
 }
 
 async function initModules() {
@@ -46,7 +44,7 @@ let moduleFiles = glob.sync(path.join(__dirname + '/../modules/*.js'))
   .concat(glob.sync(path.join(__dirname + '../modules/*/init.js')));
 
 (async () => {
-  await loadModules(moduleFiles);
+  await Promise.all(moduleFiles.map(loadModule));
   await initModules();
   await client.login(TOKEN);
 
