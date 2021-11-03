35b056c0e148900eef3a5e17ed7fe9b5722debff
optimisticside <47266822+optimisticside@users.noreply.github.com>
2021-11-03 22:28:57 +0000

    Added stats command

--- src/commands/util/stats.js
+++ src/commands/util/stats.js
@@ -7,19 +7,30 @@ const Command = require('../../structs/command.js');
 const OptionParser = require('../../util/optionParser.js');
 const { warning } = require('../../util/formatter.js')('Invite Command');
 const { NAME, PREFIX, DEFAULT_COLOR } = require('../../config.json');
+const process = require('process');
+const os = require('os');
+const GIGA_BYTE = Math.pow(1024, 3);
 
 module.exports = class InfoCommand extends Command {
   async run(client, given, args) {
+    // TODO: Clean up this code.
+    const usedMem = Math.round(((os.totalmem() - os.freemem()) / GIGA_BYTE) * 1000) / 1000;
+    const totalMem = Math.round((os.totalmem() / GIGA_BYTE) * 1000) / 1000;
+    const uptime = new Date(process.uptime() * 1000).toISOString().substr(11, 8);
+
     const embed = new MessageEmbed()
       .setTitle(NAME)
       .setColor(DEFAULT_COLOR)
-      .setDescription(`Hello! I'm ${NAME}. I was made with node.js using the Discord.js library. I was created to be convenient and easy-to-use. I am running ${client.shard.ids.length.toString()} shard${client.shard.ids.length === 1 ? '' : 's'}.`);
+      .addField('Uptime', `${uptime}`, true)
+      .addField('Memory Usage', `${usedMem} GB / ${totalMem} GB`, true)
+      .addField('Guilds', client.guilds.cache.size.toString(), true) // This will do for now.
+      .setFooter(`PID: ${process.pid} | Shard: ${client.shardId}`);
     given.reply({ embeds: [ embed ] });
   }
 
   constructor() {
     super({
-      name: 'info',
+      name: 'stats',
       desc: 'Provides information about the bot\'s state.',
       perms: [ Permissions.FLAGS.SEND_MESSAGES ],
     });
--- src/core/shard.js
+++ src/core/shard.js
@@ -57,6 +57,11 @@ const initModules = async () => {
 let moduleFiles = glob.sync(path.join(__dirname + '/../modules/*.js'))
   .concat(glob.sync(path.join(__dirname + '../modules/*/init.js')));
 
+process.on('message', async message => {
+  if (message.type !== 'shardId') return;
+  client.shardId = message.data.shardId;
+});
+
 (async () => {
   await Promise.all(moduleFiles.map(loadModule));
   await initModules();
