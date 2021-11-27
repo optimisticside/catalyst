743016e8525cedfa9c69c21bd616d9ab629e0416
optimisticside <47266822+optimisticside@users.noreply.github.com>
2021-11-27 21:27:47 +0000

    Added Statcord support

---
+++ src/services/statcord.js
@@ -0,0 +1,28 @@
+// Catalyst
+// Copyright 2021 Catalyst contributors
+// See LICENSE for details
+
+const { STATCORD_TOKEN, CLIENT_ID, STATS_UPDATE_INTERVAL } = require('../config.json');
+const API_URL = 'https://api.statcord.com/v3/stats';
+const got = require('got');
+
+module.exports = async (shardingManager) => {
+  if (!STATCORD_TOKEN) return;
+
+  const updateStats = async () => {
+    const result = await Promise.all([
+      shardingManager.fetchClientValues('guilds.cache.size'),
+      shardingManager.broadcastEval(c => c.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0))
+    ]);
+    const servers = result[0].reduce((acc, count) => acc + count, 0);
+    const users = result[1].reduce((acc, count) => acc + count, 0);
+   
+    return await got.post(API_URL, {
+      json: { id: CLIENT_ID, key: STATCORD_TOKEN, servers, users }
+    }).catch(err => {
+      console.error(`Unable to post stats to StatCord: ${err}`);
+    });
+  };
+
+  setInterval(updateStats, STATS_UPDATE_INTERVAL);
+}
