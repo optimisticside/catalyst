2177bdb8122aca52fa154c41c5cf49e0a63f2bde
optimisticside <vijaysh312@gmail.com>
2021-11-28 17:41:14 +0000

    Fixed command cooldowns

---
+++ src/models/userConfig.js
@@ -0,0 +1,17 @@
+// Catalyst
+// Copyright 2021 Catalyst contributors
+// See LICENSE for details
+
+const { Schema, model } = require('mongoose');
+
+const cooldownSchema = new Schema({
+  command: { type: String, require: true, unique: true },
+  since: { type: Date, require: true }
+});
+
+const userConfigSchema = new Schema({
+  id: { type: String, require: true, unique: true },
+  cooldowns: { type: Array, of: cooldownSchema, default: [] }
+});
+
+module.exports = model("UserConfigModels", userConfigSchema);
