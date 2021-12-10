36b04327a3d1ad56b6562f33bbd968332218e5b6
optimisticside <vijaysh312@gmail.com>
2021-12-10 18:58:55 +0000

    Started work on permission system

---
+++ src/commands/test/group.js
@@ -0,0 +1,42 @@
+// Catalyst
+// Copyright 2021 Catalyst contributors
+// See LICENSE for details
+
+const { Permissions } = require('discord.js');
+const { alert, success } = require('../../util/formatter.js')('Ban Command');
+const Command = require('../../structs/command.js');
+const OptionParser = require('../../util/optionParser.js');
+
+module.exports = class BanCommand extends Command {
+  async run(client, given, args) {
+    const parser = new OptionParser(this, given, args);
+    const id = await parser.getOption('target');
+
+		given.guild.bans.remove(id, `Unbanned by ${given.author.id}`).then(() => {
+		  given.reply(success(`Successfully unbanned user`));
+    }).catch(err => {
+      given.reply(alert(`Unable to ban user`));
+      console.log(`Unable to unban ${id}: ${err}`);
+    });
+  }
+
+  constructor() {
+    super({
+      name: 'unban',
+      desc: 'Unbans the provided user.',
+      perms: [ Permissions.FLAGS.BAN_MEMBERS ],
+      tags: [ 'moderation' ],
+      guildOnly: true,
+      passive: false,
+      options: [
+        {
+          name: 'target',
+          type: 'string',
+          desc: 'The ID of the user to ban.',
+          prompt: 'What is the ID of the user to unban?',
+          required: true
+        },
+      ]
+    })
+  }
+};
