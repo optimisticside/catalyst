2229aca9724051d0979c8dbd16658462996da479
optimisticside <47266822+optimisticside@users.noreply.github.com>
2021-12-11 17:51:16 +0000

    Cleaned up fun commands code

--- src/commands/fun/dadjoke.js
+++ src/commands/fun/dadjoke.js
@@ -2,17 +2,16 @@
 // Copyright 2021 Catalyst contributors
 // See LICENSE for details
 
-const { Permissions, MessageEmbed } = require('discord.js');
-const { neutral } = require('../../util/formatter.js')('Purge Command');
+const { Permissions } = require('discord.js');
+const { neutral } = require('../../util/formatter.js')('DadJoke Command');
 const Command = require('../../structs/command.js');
-const OptionParser = require('../../util/optionParser.js');
 const got = require('got');
 
 module.exports = class DogCommand extends Command {
   async run(client, given, args) {
     const res = await got('https://icanhazdadjoke.com/', { headers: { 'Accept': 'application/json' } });
     const joke = JSON.parse(res.body)?.joke;
-    
+
     given.reply(neutral(joke));
   }
 
@@ -22,6 +21,6 @@ module.exports = class DogCommand extends Command {
       desc: 'Sends a random dad joke.',
       perms: [ Permissions.FLAGS.SEND_MESSAGES ],
       tags: [ 'fun' ]
-    })
+    });
   }
 };
