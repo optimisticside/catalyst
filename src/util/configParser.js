7411287356f5a37b71a16f9f14dc96edcc29e14b
optimisticside <47266822+optimisticside@users.noreply.github.com>
2021-12-21 20:59:59 +0000

    Fixed config parser

--- src/util/configParser.js
+++ src/util/configParser.js
@@ -19,8 +19,8 @@ const elemHandlers = {
 
 const readElem = (name, data, spec) => {
   const type = spec.type ?? spec ?? 'string';
-  if (data == null) {
-    if (spec.default) return spec.default;
+  if (!data) {
+    if (spec.default !== null) return spec.default;
     if (spec.require) throw new Error(`${name} is a required config entry`);
   }
   return elemHandlers[type](data, spec.of);
@@ -28,10 +28,9 @@ const readElem = (name, data, spec) => {
 
 const parseConfig = (env, specs) => {
   let result = {};
-  Object.entries(env).map(([ name, data ]) => {
-    const spec = specs[name];
-    if (!spec) return;
-    result[name] = readElem(name, data, spec)
+  Object.entries(specs).map(([ name, spec ]) => {
+    const data = env[name];
+    result[name] = readElem(name, data, spec);
   });
   return result;
 }
