93c0555c66360d94d4037e1e37738cdf034858de
optimisticside <vijaysh312@gmail.com>
2022-01-06 20:14:29 +0000

    Cleaned up code

--- src/commands/util/stats.ts
+++ src/commands/util/stats.ts
@@ -5,8 +5,9 @@
 import { ColorResolvable, MessageEmbed, Permissions } from 'discord.js';
 import Command, { CommandGiven } from 'structs/command';
 import config from 'core/config';
-import * as process from 'process';
 import CatalystClient from 'core/client';
+import * as process from 'process';
+import * as os from 'os';
 
 const GIGA_BYTE = Math.pow(1024, 3);
 const { NAME, DEFAULT_COLOR } = config;
@@ -27,9 +28,11 @@ const toHrMinSec = (time: number) => {
 
 export default class StatsCommand extends Command {
   async run(client: CatalystClient, given: CommandGiven) {
-    const memoryUsage = process.memoryUsage();
-    const usedMem = Math.round((memoryUsage.heapUsed / GIGA_BYTE) * 1000) / 1000;
-    const totalMem = Math.round((memoryUsage.heapTotal / GIGA_BYTE) * 1000) / 1000;
+    const mem = process.memoryUsage();
+    const load = os.loadavg();
+
+    const usedMem = (mem.heapUsed / GIGA_BYTE).toFixed(3);
+    const totalMem = (mem.heapTotal / GIGA_BYTE).toFixed(3);
     const uptime = toHrMinSec(process.uptime() * 1000);
 
     const result = (await Promise.all([
@@ -47,7 +50,7 @@ export default class StatsCommand extends Command {
       .addField('Guilds', totalGuilds.toString(), true)
       .addField('Users', totalUsers.toString(), true)
       .addField('Uptime', `${uptime}`, true)
-      //.addField('Load Avg', os.loadavg().map(n => n.toFixed(3)).join(', '))
+      .addField('Avg Load', load.map(n => n.toFixed(3)).join(', '))
       .addField('Memory Usage', `${usedMem} GB / ${totalMem} GB`, true)
       .setFooter({ text: `PID: ${process.pid} | Cluster: ${client.shard?.id} | Shard: ${client.shard?.shardCount}` });
     given.reply({ embeds: [embed] });
