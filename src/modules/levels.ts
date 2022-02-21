5ef194992fbd4be5cf16a5bd069a46b4522ddb52
optimisticside <vijaysh312@gmail.com>
2022-02-21 17:51:21 +0000

    Finished leveling system

--- src/modules/levels.ts
+++ src/modules/levels.ts
@@ -13,6 +13,7 @@ import EventsModule from '@modules/events';
 export interface MemberData {
   level: number;
   lastUpdate: number;
+  dirty: boolean;
   needed: number;
   xp: number;
 }
@@ -20,17 +21,25 @@ export interface MemberData {
 export default class LevelModule extends Module {
   dataCache = new Map<string, Map<string, MemberData>>();
 
-  calculateNeeded(level: number) {
+  calcNeededDelta(level: number) {
     return 5 * Math.pow(level, 2) + 50 * level + 100;
   }
 
-  calculateLevel(xp: number) {
+  calcLevel(xp: number) {
     let level = 0;
+    while (xp >= this.calcNeededDelta(level)) {
+      xp -= this.calcNeededDelta(level);
+      level++;
+    }
+    return level;
+  }
+
+  calcNeeded(level: number) {
+    let xp = 0;
     do {
-      xp -= this.calculateNeeded(level);
-      if (xp < 0) return level;
-    } while (++level);
-    return 0;
+      xp += this.calcNeededDelta(level);
+    } while (--level);
+    return xp;
   }
 
   async handleMessage(message: Message) {
@@ -41,12 +50,12 @@ export default class LevelModule extends Module {
       const userData: UserDocument =
         (await UserData.findOne({ id: message.author.id })) ?? (await UserData.create({ id: message.author.id }));
 
-      console.log(userData);
       userData.xpData.forEach((xp, guildId) => {
-        const level = this.calculateLevel(xp);
+        const level = this.calcLevel(xp);
         userLevels.set(guildId, {
           lastUpdate: 0,
-          needed: this.calculateNeeded(level + 1),
+          needed: this.calcNeeded(level + 1),
+          dirty: false,
           level,
           xp
         });
@@ -55,14 +64,14 @@ export default class LevelModule extends Module {
       this.dataCache.set(message.author.id, userLevels);
     }
 
-    console.log(this.dataCache);
     const userLevels = this.dataCache.get(message.author.id);
     if (!userLevels) return; // Make typescript happy.
 
     if (!userLevels.has(message.guild.id)) {
       userLevels.set(message.guild.id, {
         lastUpdate: 0,
-        needed: this.calculateNeeded(1),
+        needed: this.calcNeeded(1),
+        dirty: false,
         level: 0,
         xp: 0
       });
@@ -71,13 +80,13 @@ export default class LevelModule extends Module {
     const levelData = userLevels.get(message.guild.id);
     if (!levelData) return; // Make typescript happy.
 
-    //if (Date.now() - levelData.lastUpdate < 60_000) return;
+    if (Date.now() - levelData.lastUpdate < 60_000) return;
     levelData.xp += (Math.abs(await randomNumber()) + 50) % 100;
+    levelData.dirty = true;
     levelData.lastUpdate = Date.now();
 
-    console.log(levelData);
     if (levelData.xp < levelData.needed) return;
-    levelData.needed += this.calculateNeeded(levelData.level++);
+    levelData.needed = this.calcNeeded(levelData.level++);
 
     const guildData: GuildDocument =
       (await GuildData.findOne({ id: message.guild.id })) ?? (await GuildData.create({ id: message.guild.id }));
@@ -92,10 +101,15 @@ export default class LevelModule extends Module {
   }
 
   async updateData() {
-    this.dataCache.forEach(async (levelData, userId) => {
+    this.dataCache.forEach(async (userLevels, userId) => {
       const xpData = new Map<string, number>();
-      levelData.forEach((levelData, guildId) => xpData.set(guildId, levelData.xp));
-      console.log('UPDATE', userId, xpData);
+      userLevels.forEach((levelData, guildId) => {
+        if (!levelData.dirty) return;
+        levelData.dirty = false;
+        xpData.set(guildId, levelData.xp)
+      });
+
+      if (!Object.entries(xpData).length) return;
       await UserData.findOneAndUpdate({ id: userId }, { xpData });
     });
   }
