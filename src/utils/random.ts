b37b473d27e1e854c2475589565aa9964caa3f29
optimisticside <47266822+optimisticside@users.noreply.github.com>
2022-02-21 17:32:14 +0000

    Add types

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -6,11 +6,16 @@ import * as crypto from 'crypto';
 
 export default async function randomNumber(nbytes = 4) {
   // Pack is just a helper function to pack a tuple into an array so it can be returned.
+  // I'm not good enough at typescript to use generics here.
   const pack =
-    (fn: (...args: any[]) => any) => // eslint-disable-line @typescript-eslint/no-explicit-any
-    (...args: any[]) => // eslint-disable-line @typescript-eslint/no-explicit-any
+    (
+      fn: (...args: any[]) => any // eslint-disable-line @typescript-eslint/no-explicit-any
+    ) =>
+    (
+      ...args: any[] // eslint-disable-line @typescript-eslint/no-explicit-any
+    ) =>
       fn([...args]);
-  const [err, buf] = await new Promise(res => crypto.randomBytes(nbytes, pack(res)));
+  const [err, buf] = (await new Promise(res => crypto.randomBytes(nbytes, pack(res)))) as [Error, Buffer];
   if (err) return Promise.reject(err);
 
   let num = 0;
